022d0583d4d3d3827261e817698778d0fb71da55
Adrien <adrien@MBP-de-Adrien.lan>
2023-12-06 21:25:15 +0100

    Copie main vers adrien

--- strat_live.py
+++ strat_live.py
@@ -1,6 +1,11 @@
+from pickletools import long1
 import time
 from api import *
+import pandas as pd
+
+
 df = None
+live_trade = False
 
 def create_trading_logic():
     return {'stop_flag': False}
@@ -8,25 +13,58 @@ def create_trading_logic():
 def start_trade(trading_logic):
     while not trading_logic['stop_flag']:
         print("Live trading is running")
-        SimpleSMALive()
-        time.sleep(2)
+        SimpleSMALive("BTC/USDT","5m",5)
+        #time.sleep(2)
     print("Live trading is stopped")
 
 def stop_trade(trading_logic):
     trading_logic['stop_flag'] = True
 
+# TODO extraire la stratégie de la onction suivante,
+# TODO 
 def SimpleSMALive(pair, timeframe, sma):
+    global df
+    global live_trade
     if df is None:
         df = getOHLCV(pair, timeframe, limit=sma+1)
-    df = df + getOHLCV(pair, timeframe, limit=1)
-    df['SMA'].df['Close'].rolling(sma).mean() #A test avec open high et low
-    last_value = df['close'].iloc[-1]
-    last_sma = df['SMA'].iloc[-1]
+
+    df = pd.concat([df, getOHLCV(pair, timeframe, limit=1)], ignore_index=True)
+    # print(df)
+    df = df.drop_duplicates(subset=['Timestamp'], keep='last')
+
+    # print(df)
+    df['SMA'] = df['Close'].rolling(sma).mean()
+    
+    last_value = df['Close'].iloc[-1]
+    last_sma = df['SMA'].iloc[-2]
+    print(f"SMA {last_sma} CLose {last_value}")
+    # print(df)
     if last_sma is None:
+        print("last sma null")
         return
-    if  last_value > last_sma:
-        
+    
+    if live_trade is False:
+        print("live_trade false")
 
+        if  last_value > last_sma:
 
+            if getQuantity(pair,"buy")>0:
+                print("lunch buy order")
+                #place_order(pair, "buy", 6, "market")
+                live_trade = True
+
+            else:
+                print("Not enought founds") 
+
+    elif last_value < last_sma:
+        if getQuantity(pair,"sell")>0:
+                print("lunch sell order")
+                #place_order(pair, "sell", 6, "market")
+                live_trade = False
 
     return 
+
+last_value = 0
+last_sma =0
+
+# SimpleSMALive("BTC/USDT", "5m", 10)
